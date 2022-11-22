10cdaf7ad94b760442ce7c22382a17440a88645b
Kosakan <kosakan27@hotmail.fr>
2022-11-22 16:42:16 +0100

    Class PlayerGrid

--- src/modele/PlayerGrid.java
+++ src/modele/PlayerGrid.java
@@ -1,8 +1,17 @@
 package modele;
 
+/**
+ * @author Kularajasingam Kosakan
+ */
 public class PlayerGrid extends Grid{
     public PlayerGrid(){
         this.mat;
 
+    }
+    public void alea() {
+        for(int i=0; i<tab.length; i++){
+            for(int j=0; j<tab.length; j++)
+                int random = (int)
+        }
     }
 }
