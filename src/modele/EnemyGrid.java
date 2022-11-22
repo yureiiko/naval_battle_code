6bad490cc62a7101cbd224eae443766771a6cb3e
zainabmouhamad <zainab.mouhamadissouf@gmail.com>
2022-11-22 16:37:34 +0100

    method that permit to fill the matrice and to display

--- src/modele/EnemyGrid.java
+++ src/modele/EnemyGrid.java
@@ -1,14 +1,8 @@
 package modele;
 
-/**
- * class EnemyGrid extends Grid{
- * @author JOSEPH PACES Anoushka
- */
 public class EnemyGrid extends Grid{
 
     public EnemyGrid(){
-
-        this.mat=mat;
-
+        this.mat
     }
 }
--- src/modele/Grid.java
+++ src/modele/Grid.java
@@ -1,7 +1,27 @@
 package modele;
 
 public class Grid {
-    String mat[][]= new String[8][8];//Matrice 8*8
-    public void remplir;
+    String mat[][]= new String[14][14];//Matrice 8*8
+
+    public void fill(){
+
+        for (int i=0;i<14;i++){
+            for(int j=0;j<14;j++){
+                mat[i][j]="#";
+            }
+        }
+    }
+
+    @Override
+    public String toString() {
+        String out ="";
+        for (int i=0;i<14;i++) {
+            for (int j = 0; j < 14; j++) {
+                out =out + mat[i][j];
+            }
+            out=out+"\n";
+        }
+        return out ;
+    }
 }
 
