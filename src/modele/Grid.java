a36fe1370e82ef065c46e927f6544ac72f9c9745
zainabmouhamad <zainab.mouhamadissouf@gmail.com>
2022-11-23 16:42:57 +0100

    method that permit to fill the matrice and to display

--- src/modele/Grid.java
+++ src/modele/Grid.java
@@ -7,13 +7,13 @@ public class Grid {
 
         for (int i=0;i<14;i++){
             for(int j=0;j<14;j++){
-                mat[i][j]="| |";
+                mat[i][j]="|  |";
             }
         }
     }
     @Override
     public String toString() {
-        String out ="\t|1 ||2 ||3 ||4 ||5 ||6 ||7 ||8 ||9 ||10||11||12||13||14|\n";
+        String out ="\t|1 |2 |3 |4 |5 |6 |7 |8 |9 |10|11|12|13|14|\n";
         for (int i=0;i<14;i++) {
             out=out+"\n"+(i+1)+"\t";
             for (int j = 0; j < 14; j++) {
