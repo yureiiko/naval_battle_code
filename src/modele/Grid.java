376834833b9e8d9b3a9d68abd5ab4de4b7b99db0
zainabmouhamad <zainab.mouhamadissouf@gmail.com>
2022-11-23 17:10:34 +0100

    method that permit to fill the matrice and to display

--- src/modele/Grid.java
+++ src/modele/Grid.java
@@ -1,25 +1,27 @@
 package modele;
 
+/**
+ * @author Mouhamad Zainab
+ */
 public class Grid {
-    String mat[][]= new String[14][14];//Matrice 8*8
+    String mat[][]= new String[15][15];//Matrice 8*8
 
-    public void fill(){
+    public void fill(){//method that fill the grid
 
-        for (int i=0;i<14;i++){
-            for(int j=0;j<14;j++){
+        for (int i=0;i<15;i++){
+            for(int j=0;j<15;j++){
                 mat[i][j]="| ";
             }
         }
     }
     @Override
-    public String toString() {
-        String out ="\t|1 |2 |3 |4 |5 |6 |7 |8 |9 |10|11|12|13|14|\n";
-        for (int i=0;i<14;i++) {
+    public String toString() {//method that permit to display the grid
+        String out ="\t|1 |2 |3 |4 |5 |6 |7 |8 |9 |10|11|12|13|14|15|\n";
+        for (int i=0;i<15;i++) {
             out=out+"\n"+(i+1)+"\t";
-            for (int j = 0; j < 14; j++) {
+            for (int j = 0; j < 15; j++) {
                 out =out + mat[i][j]+" ";
             }
-
         }
         return out ;
     }
