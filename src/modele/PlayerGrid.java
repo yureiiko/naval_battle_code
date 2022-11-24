281385727b8055c03c37e1c31db5bf2036c22971
Kosakan <kosakan27@hotmail.fr>
2022-11-24 16:50:40 +0100

    màj playerGrid

--- src/modele/PlayerGrid.java
+++ src/modele/PlayerGrid.java
@@ -17,4 +17,15 @@ public class PlayerGrid extends Grid{
         int lig = new Random().nextInt(14);
         mat[lig][col]="|#";
     }
+    public boolean check(){
+        for (int i=0;i<15;i++){
+            for(int j=0;j<15;j++){
+                if(mat[i][j]!="| "){
+                    return false;
+                }
+                }
+
+            }
+        return true;
+    }
 }
