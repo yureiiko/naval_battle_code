a2e5efa982f726e08c0c71a7fc3d38e5636d6fce
yureiiko <pierrefcz@gmail.com>
2022-11-29 14:49:35 +0100

    Minor modification

--- src/view/ConsoleDisplay.java
+++ src/view/ConsoleDisplay.java
@@ -9,7 +9,7 @@ public class ConsoleDisplay {
     /**
      * Method display
      * @param s String
-     * Display the given parameter in the console
+     * Display the given parameter in the console thanks to System.out
      */
     public void display(String s) {
         System.out.print(s);
