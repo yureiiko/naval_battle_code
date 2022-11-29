6ac76be620ae6763a77181e752dcbaade70a5800
yureiiko <pierrefcz@gmail.com>
2022-11-29 14:40:55 +0100

    Add class ConsoleDisplay in the package ```view```.

---
+++ src/view/ConsoleDisplay.java
@@ -0,0 +1,18 @@
+package view;
+
+/**
+ * Class ConsoleDisplay
+ * @author FLEURY CALAIS Camille
+ */
+public class ConsoleDisplay {
+
+    /**
+     * Method display
+     * @param s String
+     * Display the given parameter in the console
+     */
+    public void display(String s) {
+        System.out.print(s);
+    }
+
+}
