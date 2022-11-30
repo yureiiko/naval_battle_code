fe1c9e627d830babcf431df3940b7472cb1091ce
Anoushka279 <118826871+Anoushka279@users.noreply.github.com>
2022-11-30 16:25:29 +0100

    Method fire that write where the player hit

--- src/modele/EnemyGrid.java
+++ src/modele/EnemyGrid.java
@@ -7,6 +7,7 @@ public class EnemyGrid extends Grid{
 
     /**
      * Constructor
+     * fill matrix
      */
     public EnemyGrid(){
         this.fill();
@@ -17,6 +18,7 @@ public class EnemyGrid extends Grid{
      * @param l int
      * @param c int
      * @param toAdd String
+     * Update EnemyGrid
      */
     public void update(int l, int c, String toAdd) {
         mat[l][c] = toAdd;
